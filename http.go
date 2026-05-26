11aa33647fabe90e76a5a0202faf86b2c3a05f71
Ilguiz Latypov <ilatypov@yahoo.ca>
2026-05-26 04:13:05 +0000

    Order URL handlers.  Catch some pre-CIFS WebDAV lookups.

--- http.go
+++ http.go
@@ -19,15 +19,133 @@ func generateToken() string {
 	return hex.EncodeToString(token[:])
 }
 
+var socketioLocation = "/socket.io"
+var socketioPathRe = regexp.MustCompile(`^` + regexp.QuoteMeta(socketioLocation) + `(/|$)`)
 var historyPathRe = regexp.MustCompile(`^/([a-fA-F0-9]{28})/history$`)
-var tokenPathRe = regexp.MustCompile(`^/([a-fA-F0-9]{28})$`)
-var pathPingRe = regexp.MustCompile(`^/p/([a-fA-F0-9]{28})(/|$)`)
+var tokenPathRe = regexp.MustCompile(`^/([a-fA-F0-9]{28})(/|)$`)
+var pathPingRe = regexp.MustCompile(`^/p(|/)([a-fA-F0-9]{28})(/|$)`)
 var acmeLocation = "/.well-known/acme-challenge/"
 var acmeRe = regexp.MustCompile(`^` + regexp.QuoteMeta(acmeLocation) + `([a-zA-Z0-9_-]+)$`)
 var levar = "/var/lib/letsencrypt"
 
+
+func handleHttpPing(w http.ResponseWriter, r *http.Request, path string, token string, ret chan<- Record) {
+    ip := r.RemoteAddr
+    if v, ok := r.Header["X-Forwarded-For"]; ok && len(v) > 0 {
+        ip = v[0]
+    }
+    host := r.Host
+    if v, ok := r.Header["X-Real-Host"]; ok && len(v) > 0 {
+        host = v[0]
+    }
+    header := NewRecordHeader(ip, token, "http", nil)
+    ret <- &HttpRecord{
+        RecordHeader: header,
+        Domain:		  host,
+        Path:		  path,
+        Headers:	  r.Header,
+    }
+}
+
+func handleUI(w http.ResponseWriter, r *http.Request, token string, appHtml *template.Template, httpHost string, httpHostPort string) {
+    err := appHtml.Execute(w, &struct {
+        HttpHost		string
+        HttpHostPort	string
+        Token	string
+        History []Record
+    }{httpHost, httpHostPort, token, History(token)})
+    if err != nil {
+        log.Println(err)
+    }
+}
+
+func handleHistory(w http.ResponseWriter, r *http.Request, token string) {
+    history := History(token)
+    if history == nil {
+        history = []Record{}
+    }
+    j, err := json.Marshal(history)
+    if err != nil {
+        log.Println(err)
+    } else {
+        w.Write(j)
+    }
+}
+
+func handleWalkIn(w http.ResponseWriter, r *http.Request) {
+    token := generateToken()
+    http.Redirect(w, r, "/" + token, 302)
+}
+
+func handleAcme(w http.ResponseWriter, r *http.Request, path string) {
+    // The following reads from /var/lib/letsencrypt/.well-known/acme-challenge/NNN,
+    // assuming that certbot runs as follows and levar has the same value as webroot-path.
+    //  certbot --webroot --webroot-path /var/lib/letsencrypt
+    fpath := levar + path
+    http.ServeFile(w, r, fpath)
+}
+
+type httpResponder struct {
+    sockio *socketio.Server
+    ret chan<- Record
+    appHtml *template.Template
+    httpHost string
+    httpHostPort string
+}
+
+// At the time of the incoming HTTP request, choose how to process it.
+// Thanks icza https://stackoverflow.com/questions/30474313/how-to-use-regexp-get-url-pattern-in-golang
+func (responder *httpResponder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+    path := r.URL.Path
+
+    matches := pathPingRe.FindStringSubmatch(path)
+    if len(matches) == 4 {
+        // When beaconing with a web request such as with SSRF or RCE curl: "GET /p/HEX28"
+        // When beaconing with access to a share name \\SERVER\pHEX28\FILE.EXT via Microsoft WebDAV: "ANYTHING /pHEX28"
+        handleHttpPing(w, r, path, matches[2], responder.ret)
+        return
+    }
+
+    matches = tokenPathRe.FindStringSubmatch(path)
+    if len(matches) == 3 {
+        if (r.Method == "PROPFIND") || (r.Method == "OPTIONS") {
+            // When beaconing with access to a share name \\SERVER\HEX28\FILE.EXE via Microsoft WebDAV:
+            // "PROPFIND /HEX28/" and "OPTIONS /HEX28/"
+            handleHttpPing(w, r, path, matches[1], responder.ret)
+        } else {
+            // When getting redirected from a "walk-in"
+            handleUI(w, r, matches[1], responder.appHtml, responder.httpHost, responder.httpHostPort)
+        }
+        return
+    }
+
+    matches = historyPathRe.FindStringSubmatch(path)
+    if len(matches) == 2 {
+        handleHistory(w, r, matches[1])
+        return
+    }
+
+    if socketioPathRe.MatchString(path) {
+        responder.sockio.ServeHTTP(w, r)
+        return
+    }
+
+    matches = acmeRe.FindStringSubmatch(path)
+    if len(matches) == 2 {
+        handleAcme(w, r, path)
+        return
+    }
+
+    if path == "/favicon.ico" {
+        http.Error(w, "Not found", 404)
+        return
+    }
+
+    handleWalkIn(w, r)
+}
+
 func Http(listen string, httpHost string) (<-chan Record, error) {
-	ret := make(chan Record)
 	sockio := socketio.NewServer(nil)
 	sockio.OnEvent("/", "subscribe", func(s socketio.Conn, topic string) {
 		events := Subscribe(topic)
@@ -45,7 +163,6 @@ func Http(listen string, httpHost string) (<-chan Record, error) {
 			}
 		}()
 	})
-	http.Handle("/socket.io/", sockio)
 	appHtml, err := template.ParseFiles("templates/app.html")
 	if err != nil {
 		log.Fatal(err)
@@ -74,84 +191,12 @@ func Http(listen string, httpHost string) (<-chan Record, error) {
 		httpHostPort = httpHost + ":" + strconv.Itoa(port)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-		path := r.URL.Path
-		if path == "/favicon.ico" {
-			return
-		}
-		matches := historyPathRe.FindStringSubmatch(path)
-		if len(matches) > 1 {
-			token := matches[1]
-			history := History(token)
-			if history == nil {
-				history = []Record{}
-			}
-			j, err := json.Marshal(history)
-			if err != nil {
-				log.Println(err)
-			} else {
-				w.Write(j)
-			}
-			return
-		}
-		if !tokenPathRe.MatchString(path) {
-			token := generateToken()
-			http.Redirect(w, r, "/"+token, 302)
-		} else {
-			token := path[1:]
-			err = appHtml.Execute(w, &struct {
-				HttpHost		string
-				HttpHostPort	string
-				Token	string
-				History []Record
-			}{httpHost, httpHostPort, token, History(token)})
-			if err != nil {
-				log.Println(err)
-			}
-		}
-	})
-	http.HandleFunc("/p/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		matches := pathPingRe.FindStringSubmatch(path)
-		var token string
-		if len(matches) < 2 {
-			return
-		}
-		token = matches[1]
-		ip := r.RemoteAddr
-		if v, ok := r.Header["X-Forwarded-For"]; ok && len(v) > 0 {
-			ip = v[0]
-		}
-		host := r.Host
-		if v, ok := r.Header["X-Real-Host"]; ok && len(v) > 0 {
-			host = v[0]
-		}
-		header := NewRecordHeader(ip, token, "http", nil)
-		ret <- &HttpRecord{
-			RecordHeader: header,
-			Domain:		  host,
-			Path:		  path,
-			Headers:	  r.Header,
-		}
-		if err != nil {
-			log.Println(err)
-		}
-	})
-	http.HandleFunc(acmeLocation, func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-		path := r.URL.Path
-        acmeMatches := acmeRe.FindStringSubmatch(path)
-        if len(acmeMatches) == 2 {
-            // The following reads from /var/lib/letsencrypt/.well-known/acme-challenge/NNN,
-            // assuming that certbot runs as follows and levar has the same value as webroot-path.
-            //  certbot --webroot --webroot-path /var/lib/letsencrypt
-            fpath := levar + path
-            http.ServeFile(w, r, fpath)
-        } else {
-            http.Error(w, "unexpected ACME location format", 404)
-        }
-    })
+    // Create a channel that receives HTTP pings.  Return it at the end of this
+    // setup to allow publishing the ping events to subscribers.
+    ret := make(chan Record)
+    // Set up a single handler for all incoming HTTP requests.
+    http.Handle("/", &httpResponder{sockio, ret, appHtml, httpHost, httpHostPort})
+
 	go func() {
 		go sockio.Serve()
 		defer sockio.Close()
